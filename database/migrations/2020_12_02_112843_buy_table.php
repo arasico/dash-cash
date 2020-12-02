fc9ffc875ca1349c513889426e967bc4b547d811
Yaser Darzi <yaser.darzi@gmail.com>
2020-12-02 15:02:49 +0330

    first commit

---
+++ database/migrations/2020_12_02_112843_buy_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buy', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('symbol');
+            $table->decimal('quantity');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buy');
+    }
+}
