04f9a7d4e7a1c9a5a7171d8e2ea6fc86874cceb3
yaser <yaser.darzi@gmail.com>
2020-12-03 13:48:10 +0330

    first commit

--- app/Http/Controllers/CoinBoxController.php
+++ app/Http/Controllers/CoinBoxController.php
@@ -25,7 +25,7 @@ class CoinBoxController extends Controller
             ]]);
             $content = json_decode($response->getBody(), true);
             $amount = $value['amount'] - (0.1 * $value['amount']) / 100;
-            $amount = number_format($amount, 4);
+            $amount = number_format(floatval($amount), 4);
             $value['amount'] = $amount;
             $binanceResult = [
                 'current_price' => $content['lastPrice'],
